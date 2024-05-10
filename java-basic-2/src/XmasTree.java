b213f46f84bb8495065d17aaa6ad9d765a26f827
uuuuuuumi <hyphen10101004@gmail.com>
2024-05-10 12:39:42 +0900

    HowOldAreYou

--- java-basic-2/src/XmasTree.java
+++ java-basic-2/src/XmasTree.java
@@ -1,49 +1,12 @@
+public class XmasTree {
+    public static void main(String[] args) {
+        int X=Integer.parseInt(args[0]);
+        int Y=Integer.parseInt(args[1]);
 
-    public class XmasTree {
-        public static void main(String[] args) {
-            // クリスマスツリーの描画
-            drawXmasTree();
-        }
-    
-        private static void drawXmasTree() {
-            int N = 10;
-            for (int j = 0; j < N; j++) {
-                // 左側の@
-                for (int i = 0; i <= N - j; i++) {
-                    System.out.print("@");
-                }
-                // 中央の*
-                for (int i = 0; i <= j * 2; i++) {
-                    System.out.print("*");
-                }
-                // 右側の@
-                for (int i = 0; i <= N - j; i++) {
-                    System.out.print("@");
-                }
-                System.out.print("\n");
-            }
-    
-            // 幹の描画
-            drawMiki();
-        }
-    
-        private static void drawMiki() {
-            int N = 10;
-            int mikiHeight = 8;
-            for (int i = 0; i < mikiHeight; i++) {
-                // 左側の空白
-                for (int j = 0; j < N - 1; j++) {
-                    System.out.print(" ");
-                }
-                // 幹の描画
-                for (int j = 0; j < 3; j++) {
-                    System.out.print("#");
-                }
-                System.out.println();
-            }
-        }
-    
-    
+
+        System.out.println("Draw your own xmas tree with multi-loop!");
+
+
+    }
 }
-
-
+//https://docs.oracle.com/javase/jp/8/docs/api/java/lang/Integer.html
